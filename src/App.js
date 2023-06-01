dc33be7dc19b8519fe391bc4df6a393408913a9d
Manuel-Ascencio <manuelascencioprz@gmail.com>
2023-06-01 17:08:26 -0600

    change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -14,7 +14,7 @@ import CheckoutSuccess from "./components/checkout-success/CheckoutSuccess";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
@@ -28,7 +28,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
